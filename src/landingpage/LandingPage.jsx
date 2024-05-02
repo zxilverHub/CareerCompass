03dde32fc33ea427a4ed37e8fab2444c98c45164
Silver Dave Ramos <zilvdaveramos@gmail.com>
2024-05-02 13:27:39 +0800

    Update about

--- src/landingpage/LandingPage.jsx
+++ src/landingpage/LandingPage.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import {changeDomain} from '../redux/domain'
 import logo from '../assets/icons/logo.png'
 import heroImg from '../assets/images/Hero illustration.jpg'
-import findCareerImg from '../assets/images/find career.png'
+import findCareerImg from '../assets/images/About illsutration.png'
 import notfound from '../assets/images/not found.jpg'
 import './landingpage.css'
 import { useState } from 'react'
@@ -15,6 +15,29 @@ function LandingPage() {
     const mobileNavStyle = {
       top: isNavExpand? '25%': '-50%'
     }
+
+    const imgSrce = [
+
+      {
+        label: 'Career Trends',
+        img: './src/assets/images/career trends.png'
+      },
+
+      {
+        label: 'No degree careers',
+        img: './src/assets/images/no degree.png'
+      },
+
+      {
+        label: 'Discover best career for you',
+        img: './src/assets/images/discover careers.png'
+      },
+
+      {
+        label: 'Set your goals',
+        img: './src/assets/images/set goal.png'
+      }
+    ]
 
   return (
     <div id='landing-page'>
@@ -89,13 +112,13 @@ function LandingPage() {
                   <path d="M0.308162 1.08844C0.110846 1.28581 0 1.55347 0 1.83256C0 2.11165 0.110846 2.37931 0.308162 2.57669L5.5181 7.78663L0.308162 12.9966C0.116438 13.1951 0.0103501 13.4609 0.0127482 13.7369C0.0151463 14.0129 0.125838 14.2769 0.320983 14.472C0.516127 14.6671 0.78011 14.7778 1.05608 14.7802C1.33204 14.7826 1.59791 14.6765 1.79641 14.4848L7.75048 8.53075C7.9478 8.33338 8.05864 8.06572 8.05864 7.78663C8.05864 7.50754 7.9478 7.23988 7.75048 7.0425L1.79641 1.08844C1.59904 0.891119 1.33138 0.780273 1.05229 0.780273C0.773199 0.780273 0.505537 0.891119 0.308162 1.08844Z" fill="white"/>
                 </svg>
               </div>
-              <div className="cta accent">About us</div>
+              <a href='#about-us' className="cta accent">About us</a>
             </div>
           </div>
         </div>
 
         <div id="about-us" className='flex'>
-        <div className="illustration">
+          <div className="illustration">
             <img src={findCareerImg} alt="Find career image" title='Find career' />
           </div>
           <div className="about column">
@@ -124,6 +147,31 @@ function LandingPage() {
                 </svg>
             </div>
           </div>
+        </div>
+
+        <div className="discover">
+          <div className="card column">
+            <h3>Discover Careers and Career Trends</h3>
+            <p className='text'>Discover the best career for you, practice your skills, set your goals and explore career tends, jobs, and degrees the world is offered.</p>
+            <div className="cta flex">
+              Find career
+              <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0.308162 1.08844C0.110846 1.28581 0 1.55347 0 1.83256C0 2.11165 0.110846 2.37931 0.308162 2.57669L5.5181 7.78663L0.308162 12.9966C0.116438 13.1951 0.0103501 13.4609 0.0127482 13.7369C0.0151463 14.0129 0.125838 14.2769 0.320983 14.472C0.516127 14.6671 0.78011 14.7778 1.05608 14.7802C1.33204 14.7826 1.59791 14.6765 1.79641 14.4848L7.75048 8.53075C7.9478 8.33338 8.05864 8.06572 8.05864 7.78663C8.05864 7.50754 7.9478 7.23988 7.75048 7.0425L1.79641 1.08844C1.59904 0.891119 1.33138 0.780273 1.05229 0.780273C0.773199 0.780273 0.505537 0.891119 0.308162 1.08844Z" fill="white"/>
+              </svg>
+            </div>
+          </div>
+
+          { imgSrce.map((src, i) => (
+            <div className="card" key={i}>
+              <div className="img">
+                <img src={src.img} />
+              </div>
+              <div className="labels">
+                <h3>{src.label}</h3>
+              </div>
+            </div>
+            ))
+          }
         </div>
     </div>
   )
