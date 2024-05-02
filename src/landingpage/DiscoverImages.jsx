8b42b0100034a5ec001e9d9278f8577c889ac534
Silver Dave Ramos <zilvdaveramos@gmail.com>
2024-05-02 18:20:59 +0800

    add team

--- src/landingpage/DiscoverImages.jsx
+++ src/landingpage/DiscoverImages.jsx
@@ -3,4 +3,8 @@ import noDegree from './images/no degree.png'
 import discoverCareer from './images/discover careers.png'
 import setGoal from './images/set goal.png'
 
-export { careerTrends, noDegree, discoverCareer, setGoal }
+import lion1 from './team/lion 1.jpg'
+import lion2 from './team/lion 2.jpg'
+import lion3 from './team/lion 3.jpg'
+
+export { careerTrends, noDegree, discoverCareer, setGoal, lion1, lion2, lion3 }
